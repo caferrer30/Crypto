7005a74af01711847e36a7390bd1a6eca51f0c66
Carlos Ferrer <caferrer30@gmail.com>
2023-10-13 10:23:03 -0600

    Control de Errores

--- router/TOTP.py
+++ router/TOTP.py
@@ -2,11 +2,11 @@ import os
 import time
 import base64
 from fastapi import APIRouter, HTTPException, status
-from cryptography.hazmat.primitives.twofactor.totp import TOTP
+from cryptography.hazmat.primitives.twofactor.totp import TOTP, InvalidToken
 from cryptography.hazmat.primitives.hashes import SHA1, SHA256
 from pydantic import BaseModel
 
-class OTP(BaseModel):
+class VERIFY(BaseModel):
     key: str
     otp: str
     time: float
@@ -14,7 +14,7 @@ class OTP(BaseModel):
 router = APIRouter()    
 
 @router.get("/totp/generate")
-async def totp(otp: OTP, response_model=OTP, status_code=status.HTTP_200_OK):
+async def generate(status_code=status.HTTP_200_OK):
     key = os.urandom(20)
     dkey = base64.b64encode(key)
     totp = TOTP(key, 8, SHA1(), 30)
@@ -23,11 +23,15 @@ async def totp(otp: OTP, response_model=OTP, status_code=status.HTTP_200_OK):
     verify = totp.verify(totp_value, time_value)
     return {"key" : dkey, "otp": totp_value, "time" : time_value}
 
-@router.get("/totp/verify")
-async def totp(otp: OTP, response_model=OTP, status_code=status.HTTP_200_OK):
+@router.post("/totp/verify")
+async def verify(otp: VERIFY, response_model=VERIFY, status_code=status.HTTP_200_OK):
     totp_value = otp.otp.encode()
     key = base64.b64decode(otp.key)
     totp = TOTP(key, 8, SHA1(), 30)
     time_value = otp.time
-    verify = totp.verify(totp_value, time_value)
-    return {"verify": verify}
+    try:
+        verify = totp.verify(totp_value, time_value)
+        raise HTTPException(status_code=status.HTTP_200_OK,detail="Ok")
+    except InvalidToken:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="OTP Incorrecta")
+    return
